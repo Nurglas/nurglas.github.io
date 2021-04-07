dffe6057aedd38b76ece26e86999e6c8c964dd01
Nurglas <81296879+Nurglas@users.noreply.github.com>
2021-04-07 18:47:00 +0300

    Add files via upload

---
+++ project/puls/js/help_main.js
@@ -0,0 +1,81 @@
+var name = "artem";
+let number = 7;
+const pi = 3.14
+
+//number числовой тип данныйх
+//string - "", '', `` строчный помещенный в ковычки
+//true/felse булитовый т.е. да или нет
+//null если мы хотим получить элемент со страницы но его не существует то мы получим null
+//undefined что то существует но не имеет никакого значения
+//объект который может включать любые типы данных записывается:
+/* let obj= {
+    name: 'apple',
+    color: 'green',
+    weight: 200
+} */
+// symbol символьный тип данных
+//alert(123)
+//console.log(number)//выводит информацию в консоли 
+//let answer = confirm("вам есть 18?");
+//console.log(answer);
+
+//let answer = prompt("вам есть 18?", "");
+//console.log(answer);
+
+
+//операторы
+//console.log(4 + 'sad');
+
+//let isChecked = false,
+//    isClose = false;
+
+//console.log(isChecked && isClose);
+
+//console.log(isChecked || isClose);
+
+// условия if - если; else- иначе
+// if (2*4 == 8*3) {
+//     console.log("верно")
+// }else{
+//     console.log("ошибка")
+// }
+
+// let answer = confirm("вам есть 18?");
+// if (answer){
+//     console.log("prohodi")
+// } else{
+//     console.log("uhodi")
+// }
+
+// const num = 50;
+
+// if (num<49) {
+//     console.log('nepravilno')
+// } else if (num>100) {
+//     console.log("mnogo")
+// } else{
+//     console.log("werno")
+// }
+
+
+
+
+// цикл
+// for (let i = 1; i<8; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+// функции
+function logging(a, b){
+    console.log(a +b)
+}
+
+logging(3, 5);
+
+
+
+logging(4, 2);
